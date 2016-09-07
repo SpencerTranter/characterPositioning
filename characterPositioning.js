dd930c1ec3556eff8aed0c55a8bcf9b81e2fdb43
Spencer Tranter <spencertranter95@gmail.com>
2016-09-07 00:13:21 +0000

    first commit with code from characterCounting

---
+++ characterPositioning.js
@@ -0,0 +1,19 @@
+var args = process.argv.slice(2);
+
+function countLetters(word){
+  var splitWord = word.join('').toLowerCase().split('');
+
+  var obj = {};
+    for(var i = 0; i < splitWord.length; i++){
+      if (!obj.hasOwnProperty(splitWord[i])){
+        obj[splitWord[i]] = 1;
+      } else {
+        obj[splitWord[i]] ++;
+      }
+    }
+  console.log(obj);
+
+}
+
+
+countLetters(args);
